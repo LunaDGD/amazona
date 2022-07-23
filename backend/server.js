cbd62eabb93625bf4cce63e2030a6421fdaa3e58
LunaDGD <lunadgd@protonmail.com>
2022-07-23 17:54:36 -0400

    'established mysql connection'

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import data from './data.js';
+import mysql from 'mysql';
 
 const app = express();
 // test
@@ -14,6 +15,9 @@ app.get('/api/products/slug/:slug', (req, res) => {
     res.status(404).send({ message: 'Product Not Found' });
   }
 });
+app.get('/', (req, res) => {
+  res.send('hello');
+});
 app.get('/api/products/:id', (req, res) => {
   const product = data.products.find((x) => x._id === req.params.id);
   if (product) {
@@ -26,4 +30,26 @@ app.get('/api/products/:id', (req, res) => {
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
+});
+
+// establish mysql connection and api
+const db = mysql.createConnection({
+  host: 'ecom.czv82qqakxds.us-east-1.rds.amazonaws.com',
+  port: 3306,
+  user: 'admin',
+  password: 'password',
+  database: 'ecommerce',
+});
+db.connect((err) => {
+  if (err) throw err;
+  console.log('connect to db');
+});
+//grabs db data and gives it as json object
+app.get('/api/shop', (req, res) => {
+  db.query('SELECT * FROM products', (err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.status(200).json(result);
+  });
 });
